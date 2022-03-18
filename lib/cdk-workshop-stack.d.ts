f0bf92ee09203c09f89bc11f3b3ef399d55415f8
Jose Padron <jose.padron712@gmail.com>
2022-03-18 02:12:01 -0400

    se agrega output con url de 2 endpoints a cloudformation

--- lib/cdk-workshop-stack.d.ts
+++ lib/cdk-workshop-stack.d.ts
@@ -1,5 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 export declare class CdkWorkshopStack extends Stack {
+    readonly hcViewerUrl: CfnOutput;
+    readonly hcEndpoint: CfnOutput;
     constructor(scope: Construct, id: string, props?: StackProps);
 }
