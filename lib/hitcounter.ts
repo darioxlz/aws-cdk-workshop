78488f461aac76b5850fff68a6390fae4caabc68
Jose Padron <jose.padron712@gmail.com>
2022-03-18 00:52:23 -0400

    first commit

---
+++ lib/hitcounter.ts
@@ -0,0 +1,39 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+
+export interface HitCounterProps {
+  /** the function for which we want to count url hits **/
+  downstream: lambda.IFunction;
+}
+
+export class HitCounter extends Construct {
+  /** allows accessing the counter function */
+  public readonly handler: lambda.Function;
+
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    this.table = new dynamodb.Table(this, 'Hits', {
+        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        code: lambda.Code.fromAsset('lambda'),
+        handler: 'hitcounter.handler',
+        environment: {
+            DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+            HITS_TABLE_NAME: this.table.tableName
+        }
+    });
+
+    // grant the lambda role read/write permissions to our table
+    this.table.grantReadWriteData(this.handler);
+    props.downstream.grantInvoke(this.handler);
+  }
+}
